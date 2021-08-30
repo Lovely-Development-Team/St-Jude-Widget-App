1d569bcc76d782c1bacd864bb4a44640527c69af
Justin Hamilton <jwhamilton99@gmail.com>
2021-08-30 10:38:32 -0400

    improvements in issue #13

--- Shared/Views/WidgetSettingsView.swift
+++ Shared/Views/WidgetSettingsView.swift
@@ -12,18 +12,36 @@ struct WidgetSettingsView: View {
     
     @Binding var showMilestones: Bool
     @Binding var showFullCurrencySymbol: Bool
+    var parentView: ContentView
+    
+    @State private var imageHeight: CGFloat = 0
     
     var body: some View {
         
         VStack(spacing: 15) {
-            
             HStack {
                 Image(uiImage: UIImage(named: "AppIcon") ?? UIImage())
                     .clipShape(RoundedRectangle(cornerRadius: 4, style: .continuous))
+                    .background(
+                        GeometryReader() { geometry -> Color in
+                            DispatchQueue.main.async {
+                                self.imageHeight = geometry.size.height
+                            }
+                            return Color.clear
+                        }
+                    )
     //            Text(UIApplication.displayName)
                 Text("Relay FM for St. Jude")
                     .font(.headline)
                 Spacer()
+                Button(action: {
+                    self.parentView.dismissSettings()
+                }, label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: self.imageHeight, height: self.imageHeight, alignment: .center)
+                        .foregroundColor(.secondary)
+                })
             }
             
             Text("Displays the current Relay FM for St. Jude funraising status.")
@@ -55,6 +73,6 @@ struct WidgetSettingsView: View {
 
 struct WidgetSettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        WidgetSettingsView(showMilestones: .constant(false), showFullCurrencySymbol: .constant(true))
+        WidgetSettingsView(showMilestones: .constant(false), showFullCurrencySymbol: .constant(true), parentView: ContentView())
     }
 }
