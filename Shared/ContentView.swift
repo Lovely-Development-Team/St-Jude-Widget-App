c0c711b51021be7d7b5a4ae275677c348b31a91a
Ben Cardy <ben.cardy@ocado.com>
2021-08-30 08:33:33 +0100

    Ability to change in-app 'widget' settings

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -16,9 +16,13 @@ struct ContentView: View {
     @State private var widgetData = sampleCampaign
     @AppStorage("relayData", store: UserDefaults.shared) private var storedData: Data = Data()
     @StateObject private var apiClient = ApiClient.shared
-    #if os(iOS)
+#if os(iOS)
     @State var backgroundTask: UIBackgroundTaskIdentifier = .invalid
-    #endif
+#endif
+    
+    @State private var isWidgetFlipped: Bool = false
+    @State private var showMilestones: Bool = true
+    @State private var showFullCurrencySymbol: Bool = false
     
     var body: some View {
         VStack {
@@ -35,19 +39,43 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity, alignment: .center)
                 .accessibility(label: Text("This app provides a widget to track the progress of the 2021 Relay FM Saint Jude fundraiser. Add the widget to your Home Screen!"))
             Spacer()
-            EntryView(campaign: $widgetData, showMilestones: true, showFullCurrencySymbol: true)
-                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                .frame(minWidth: 0, maxWidth: 795, minHeight: 300, maxHeight: 378.5)
-                .onAppear {
-                    #if os(iOS)
-                    submitRefreshTask()
-                    #endif
-                    do {
-                        widgetData = try apiClient.jsonDecoder.decode(TiltifyWidgetData.self, from: storedData)
-                    } catch {
-                        dataLogger.error("Failed to store API response: \(error.localizedDescription)")
-                    }
+            
+            
+            VStack {
+                if isWidgetFlipped {
+                    WidgetSettingsView(showMilestones: $showMilestones, showFullCurrencySymbol: $showFullCurrencySymbol)
+                        .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+                        .frame(minWidth: 0, maxWidth: 795, minHeight: 300, maxHeight: 378.5)
+                } else {
+                    EntryView(campaign: $widgetData, showMilestones: showMilestones, showFullCurrencySymbol: showFullCurrencySymbol)
+                        .onAppear {
+#if os(iOS)
+                            submitRefreshTask()
+#endif
+                            do {
+                                widgetData = try apiClient.jsonDecoder.decode(TiltifyWidgetData.self, from: storedData)
+                            } catch {
+                                dataLogger.error("Failed to store API response: \(error.localizedDescription)")
+                            }
+                        }
+                        .contextMenu {
+                            Button {
+                                self.isWidgetFlipped = true
+                            } label: {
+                                Label("Edit Widget", systemImage: "info.circle")
+                            }
+                        }
                 }
+            }
+            .background(Color(UIColor.secondarySystemBackground))
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            .frame(minWidth: 0, maxWidth: 795, minHeight: 300, maxHeight: 378.5)
+            .rotation3DEffect(.degrees(isWidgetFlipped ? 180 : 0), axis: (x: 0, y: 1, z: 0))
+            .animation(.spring())
+            .onTapGesture {
+                self.isWidgetFlipped = true
+            }
+            
             Spacer()
             Link("Visit the fundraiser!", destination: URL(string: "https://stjude.org/relay")!)
                 .font(.headline)
@@ -70,6 +98,19 @@ struct ContentView: View {
             }
         }
         .padding()
+        .onTapGesture {
+            self.isWidgetFlipped = false
+        }
+        .onAppear {
+            self.showMilestones = UserDefaults.shared.inAppShowMilestones
+            self.showFullCurrencySymbol = UserDefaults.shared.inAppShowFullCurrencySymbol
+        }
+        .onChange(of: showMilestones, perform: { newShowMilestones in
+            UserDefaults.shared.inAppShowMilestones = newShowMilestones
+        })
+        .onChange(of: showFullCurrencySymbol, perform: { newShowFullCurrencySymbol in
+            UserDefaults.shared.inAppShowFullCurrencySymbol = newShowFullCurrencySymbol
+        })
         .onChange(of: scenePhase) { newPhase in
             if scenePhase == .background && newPhase != .background{
                 let dataTask = apiClient.fetchCampaign { result in
@@ -84,16 +125,16 @@ struct ContentView: View {
                             dataLogger.error("Failed to store API response: \((error as? LocalizedError)?.errorDescription ?? error.localizedDescription)")
                         }
                     }
-                    #if os(iOS)
+#if os(iOS)
                     UIApplication.shared.endBackgroundTask(backgroundTask)
-                    #endif
+#endif
                 }
-                #if os(iOS)
+#if os(iOS)
                 backgroundTask = UIApplication.shared.beginBackgroundTask {
                     dataTask?.cancel()
                     UIApplication.shared.endBackgroundTask(backgroundTask)
                 }
-                #endif
+#endif
             }
         }
     }
