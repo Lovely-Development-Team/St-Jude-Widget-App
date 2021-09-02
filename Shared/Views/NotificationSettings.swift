e29d2875e0940f5aeca8c12cc7ce4794f40a6d4f
David Stephens <git@dwrs.org>
2021-09-02 15:44:38 +0100

    Rename NotificationSettingsData -> NotificationSettingsController Also some minor tweaks to the pub/sub behaviour

--- Shared/Views/NotificationSettings.swift
+++ Shared/Views/NotificationSettings.swift
@@ -9,91 +9,10 @@ import SwiftUI
 import UserNotifications
 import Combine
 
-class NotificationSettingsData: ObservableObject {
-
-    private var refreshing = false
-    private var subscribers: Set<AnyCancellable> = []
-    
-    @Published var showMilestones: Bool = false
-    
-    @Published var showGoal: Bool = false
-    
-    @Published var showSignificantAmounts: Bool = false
-    
-    @Published var showMilestoneAdded: Bool = false
-    
-    @Published var notificationsAllowed: Bool = false
-    @Published var notificationAccessAsked: Bool = false
-    
-    init() {
-        self.refresh()
-        self.setupPublishers()
-    }
-    
-    func setupPublishers() {
-        self.$showMilestones.sink { newValue in
-            if(!self.refreshing) { UserDefaults.shared.showMilestoneNotification = newValue }
-        }.store(in: &self.subscribers)
-        
-        
-        self.$showGoal.sink { newValue in
-            if(!self.refreshing) { UserDefaults.shared.showGoalNotification = newValue }
-        }.store(in: &self.subscribers)
-        
-        
-        self.$showSignificantAmounts.sink { newValue in
-            if(!self.refreshing) { UserDefaults.shared.showSignificantAmountNotification = newValue }
-        }.store(in: &self.subscribers)
-        
-        
-        self.$showMilestoneAdded.sink { newValue in
-            if(!self.refreshing) { UserDefaults.shared.showMilestoneAddedNotification = newValue }
-        }.store(in: &self.subscribers)
-    }
-    
-    func removePublishers() {
-        for subscriber in self.subscribers {
-            subscriber.cancel()
-        }
-    }
-    
-    func refresh() {
-        if(!self.refreshing) {
-            self.refreshing = true
-            removePublishers()
-            UNUserNotificationCenter.current().getNotificationSettings(completionHandler: {(settings) in
-                
-                DispatchQueue.main.async {
-                    if(settings.authorizationStatus == .authorized) {
-                        self.showMilestones = UserDefaults.shared.showMilestoneNotification
-                        self.showGoal = UserDefaults.shared.showGoalNotification
-                        self.showSignificantAmounts = UserDefaults.shared.showSignificantAmountNotification
-                        self.showMilestoneAdded = UserDefaults.shared.showMilestoneAddedNotification
-                        self.notificationsAllowed = true
-                    } else {
-                        self.showMilestones = false
-                        self.showGoal = false
-                        self.showSignificantAmounts = false
-                        self.showMilestoneAdded = false
-                        self.notificationsAllowed = false
-                    }
-                    
-                    if(settings.authorizationStatus != .notDetermined) {
-                        self.notificationAccessAsked = true
-                    }
-                    
-                    self.setupPublishers()
-                    self.refreshing = false
-                }
-            })
-        }
-    }
-}
-
 struct NotificationSettings: View {
-    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.presentationMode) private var presentationMode
     @Environment(\.scenePhase) private var scenePhase
-    @StateObject var data = NotificationSettingsData()
+    @StateObject private var data = NotificationSettingsController()
     
     var body: some View {
         NavigationView {    
@@ -142,7 +61,7 @@ struct NotificationSettings: View {
                 })
             })
             .onChange(of: scenePhase) { newPhase in
-                if scenePhase == .background && newPhase != .background{
+                if scenePhase != .active && newPhase != .background {
                     self.data.refresh()
                 }
             }
