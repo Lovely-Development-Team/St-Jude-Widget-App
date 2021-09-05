e4a2aba6100a76d74bd29f8d1459d056b749840d
Justin Hamilton <jwhamilton99@gmail.com>
2021-09-04 20:32:10 -0400

    fixed custom amount entry bugs, and other notification improvements

--- Shared/Controller/NotificationSettingsController.swift
+++ Shared/Controller/NotificationSettingsController.swift
@@ -26,8 +26,10 @@ class NotificationSettingsController: ObservableObject {
     @Published var notificationAccessAsked: Bool = false
     
     @Published var enableCustomAmountNotification: Bool = false
-    @Published var customAmountInput: String = "$100"
+    @Published var customAmountInput: String = ""
     @Published var rejectedInputShowing: Bool = false
+    
+    private var modifyingPublishers: Bool = false
     
     init() {
         self.showMilestones = UserDefaults.shared.showMilestoneNotification
@@ -44,44 +46,65 @@ class NotificationSettingsController: ObservableObject {
         self.refresh()
     }
     
+    //these four functions are for the new input system. it formats the amount with thousands separators and stuff once you're done editing, and when editing it just has the digits so the commas don't mess anything up with your input
+    func formatText() {
+        guard let number = self.getNumberFromFormattedString(self.customAmountInput) else { return }
+        guard let string = self.getFormattedStringFromNumber(number) else { return }
+        self.customAmountInput = string
+    }
+    
+    func deformatText() {
+        guard let number = self.getNumberFromFormattedString(self.customAmountInput) else { return }
+        self.customAmountInput = number.stringValue
+    }
+    
+    func getFormattedStringFromNumber(_ number: NSNumber) -> String? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = ""
+        formatter.currencySymbol = ""
+        
+        return formatter.string(from: number)
+    }
+    
+    func getNumberFromFormattedString(_ string: String) -> NSNumber? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        return formatter.number(from: string)
+    }
+    
     func setupPublishers() {
+        self.modifyingPublishers = true
         self.$showMilestones.removeDuplicates().sink { newValue in
-            if(!self.refreshing) { UserDefaults.shared.showMilestoneNotification = newValue }
+            if(!self.refreshing && !self.modifyingPublishers) { UserDefaults.shared.showMilestoneNotification = newValue }
         }.store(in: &self.subscribers)
         
         self.$showGoal.removeDuplicates().sink { newValue in
-            if(!self.refreshing) { UserDefaults.shared.showGoalNotification = newValue }
+            if(!self.refreshing && !self.modifyingPublishers) { UserDefaults.shared.showGoalNotification = newValue }
         }.store(in: &self.subscribers)
         
         
         self.$showSignificantAmounts.removeDuplicates().sink { newValue in
-            if(!self.refreshing) { UserDefaults.shared.showSignificantAmountNotification = newValue }
+            if(!self.refreshing && !self.modifyingPublishers) { UserDefaults.shared.showSignificantAmountNotification = newValue }
         }.store(in: &self.subscribers)
         
         
         self.$showMilestoneAdded.removeDuplicates().sink { newValue in
-            if(!self.refreshing) { UserDefaults.shared.showMilestoneAddedNotification = newValue }
+            if(!self.refreshing && !self.modifyingPublishers) { UserDefaults.shared.showMilestoneAddedNotification = newValue }
         }.store(in: &self.subscribers)
         
         self.$enableCustomAmountNotification.removeDuplicates().sink { newValue in
-            UserDefaults.shared.enableCustomAmountNotification = newValue
+            if(!self.refreshing && !self.modifyingPublishers) { UserDefaults.shared.enableCustomAmountNotification = newValue }
         }.store(in: &self.subscribers)
         
         
         self.$customAmountInput.sink(receiveValue: {newValue in
-            if let doubleValue = Double(newValue) {
-                //no currency formatting fallback (e.g. 300000)
-                UserDefaults.shared.customNotificationAmount = doubleValue
-                self.rejectedInputShowing = false
-            } else {
-                //currency formatter (e.g. $300,000)
-                let f = NumberFormatter()
-                f.numberStyle = .currency
-                if let number = f.number(from: newValue) {
-                    DispatchQueue.main.async {
-                        self.rejectedInputShowing = false
-                        UserDefaults.shared.customNotificationAmount = number.doubleValue
-                    }
+            if(!self.refreshing && !self.modifyingPublishers) {
+                if let numberValue = self.getNumberFromFormattedString(newValue) {
+                    //no currency formatting fallback (e.g. 300000)
+                    UserDefaults.shared.customNotificationAmount = numberValue.doubleValue
+                    self.rejectedInputShowing = false
                 } else {
                     self.rejectedInputShowing = true
                 }
@@ -90,40 +113,39 @@ class NotificationSettingsController: ObservableObject {
         
         
         self.$enableCustomAmountNotification.sink(receiveValue: {newValue in
-            UNUserNotificationCenter.current().getNotificationSettings(completionHandler: {(settings) in
-                switch(settings.authorizationStatus) {
-                case .authorized:
-                    DispatchQueue.main.async {
-                        if(newValue) {
-                            if(UserDefaults.shared.customNotificationAmount == 0.0) {
-                                self.customAmountInput = "$100.00"
-                                
-                                UserDefaults.shared.customNotificationAmount = 100
-                            } else {
-                                self.customAmountInput = formatCurrency(from: String(UserDefaults.shared.customNotificationAmount), currency: "USD", showFullCurrencySymbol: false).1
+            if(!self.refreshing && !self.modifyingPublishers) {
+                UNUserNotificationCenter.current().getNotificationSettings(completionHandler: {(settings) in
+                    switch(settings.authorizationStatus) {
+                    case .authorized:
+                        DispatchQueue.main.async {
+                            if(newValue) {
+                                print(UserDefaults.shared.customNotificationAmount)
+                                if(UserDefaults.shared.customNotificationAmount != 0.0) {
+                                    self.customAmountInput = self.getFormattedStringFromNumber(NSNumber(value: UserDefaults.shared.customNotificationAmount)) ?? "100.00"
+                                }
                             }
-                        } else {
-                            UserDefaults.shared.removeObject(forKey: UserDefaults.customNotificationAmountKey)
                         }
+                        break
+                    default:
+                        setNotificationPreference(newValue: nil, for: nil)
+                        break
                     }
-                    break
-                default:
-                    setNotificationPreference(newValue: nil, for: nil)
-                    break
-                }
-            })
+                })
+            }
         }).store(in: &self.subscribers)
+        self.modifyingPublishers = false
     }
     
     func removePublishers() {
+        self.modifyingPublishers = true
         self.subscribers = []
+        self.modifyingPublishers = false
     }
     
     func refresh() {
         if (!self.refreshing) {
             self.refreshing = true
             UNUserNotificationCenter.current().getNotificationSettings(completionHandler: {(settings) in
-                
                 DispatchQueue.main.async {
                     if(settings.authorizationStatus == .authorized) {
                         self.setupPublishers()
@@ -133,10 +155,12 @@ class NotificationSettingsController: ObservableObject {
                         self.showMilestoneAdded = UserDefaults.shared.showMilestoneAddedNotification
                         self.enableCustomAmountNotification = UserDefaults.shared.enableCustomAmountNotification
                         if self.enableCustomAmountNotification {
-                            self.customAmountInput = formatCurrency(from: String(UserDefaults.shared.customNotificationAmount), currency: "USD", showFullCurrencySymbol: false).1
+                            if(UserDefaults.shared.customNotificationAmount != 0) {
+                                self.customAmountInput = self.getFormattedStringFromNumber(NSNumber(value: UserDefaults.shared.customNotificationAmount)) ?? "100.00"
+                            }
                         }
                         self.notificationsAllowed = true
-                    } else {
+                    } else if(settings.authorizationStatus == .denied) {
                         self.removePublishers()
                         self.showMilestones = false
                         self.showGoal = false
@@ -144,14 +168,12 @@ class NotificationSettingsController: ObservableObject {
                         self.showMilestoneAdded = false
                         self.notificationsAllowed = false
                         self.enableCustomAmountNotification = false
-                        self.customAmountInput = "$100"
+                        self.customAmountInput = "100.00"
                     }
                     
                     if(settings.authorizationStatus != .notDetermined) {
                         self.notificationAccessAsked = true
                     }
-                    
-                    
                     
                     self.refreshing = false
                 }
