00d0d0f1cb1b8b331c7baefaf0e5fa44def22a0e
Rosemary Orchard <16113535+RosemaryOrchard@users.noreply.github.com>
2021-09-16 21:25:21 +0200

    Fix build script, update build description

--- automated-checks.sh
+++ automated-checks.sh
@@ -1,17 +1,41 @@
+set -ex
 FILE=build_app
-LAST_BUILD_FILE=last_build
 FILE_HASH=$(sha3sum $FILE)
-LAST_BUILD_HASH=$(cat $LAST_BUILD_FILE 2>/dev/null)
+
+LAST_BUILD_FILE=last_build
+LAST_BUILD_HASH=$(cat $LAST_BUILD_FILE || echo "") 2>/dev/null
+
+ICON_FILE=icon.png
+ICON_HASH=$(sha3sum $ICON_FILE)
+
+LAST_ICON_FILE=icon_hash
+LAST_ICON_HASH=$((cat $LAST_ICON_FILE || echo "") 2>/dev/null)
+
+CHANGE_ICON=false
+
+# PR body
+BODY=$(cat $FILE)
+
+PROJECT_FILE=$(find . -maxdepth 1 -name '*.xcodeproj')
+
 # Build file must exist and be different from the last we processed
 if test -f "$FILE" -a "$FILE_HASH" != "$LAST_BUILD_HASH"; then
+	if test -f "$ICON_FILE" -a "$ICON_HASH" != "$LAST_ICON_HASH"; then
+		CHANGE_ICON=true
+		npx app-icon generate
+		sha3sum $ICON_FILE > $LAST_ICON_HASH
+	fi 
+	touch $LAST_BUILD_FILE
 	rm $LAST_BUILD_FILE
 	# Bump the build version
 	agvtool bump
 	# Store the new build number for use in commit
 	NEW_BUILD=$(agvtool what-version -terse)
-	PROJECT_FILE=$(find . -maxdepth 1 -name '*.xcodeproj')
+	# Clean up previous builds
+	rm -rf build
+	xcodebuild clean
 	# Build the app and upload
-	xcodebuild -project "$PROJECT_FILE" -scheme "St Jude (iOS)" -configuration Release -archivePath ./app.xcarchive  archive
+	xcodebuild -project "$PROJECT_FILE" -scheme "St Jude (iOS)" -configuration Release -destination 'platform=iOS Simulator,name=iPhone 12' -archivePath ./app.xcarchive -allowProvisioningUpdates archive
 	xcodebuild -exportArchive -archivePath ./app.xcarchive -exportOptionsPlist exportOptions.plist
 	# PR with new version number
 	git checkout -B "release/$NEW_BUILD"
@@ -21,9 +45,12 @@ if test -f "$FILE" -a "$FILE_HASH" != "$LAST_BUILD_HASH"; then
 	rm $FILE
 	git add "$PROJECT_FILE/project.pbxproj"
 	git add "$FILE"
+	if "$CHANGE_ICON" == "true"; then
+		git add Elsewhen/Assets.xcassets/AppIcon.appiconset/*
+	fi
 	git commit -m "Bump build ($NEW_BUILD)"
-	git push -u orign "release/$NEW_BUILD"
-	gh pr create --title "Release $NEW_BUILD" --body "$(cat $FILE)" -B main
+	git push -u origin "release/$NEW_BUILD"
+	gh pr create --title "Release $NEW_BUILD" --body "$BODY" -B main
 	# Return to the main branch
 	git checkout main
 	# Remove app.xcarchive
