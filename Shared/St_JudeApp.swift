6cecddc72e7a1410d15542aecee273adb4fe3e58
Ben Cardy <ben.cardy@ocado.com>
2022-08-24 15:55:43 +0100

    LazyVGrid to look better on iPad

--- Shared/St_JudeApp.swift
+++ Shared/St_JudeApp.swift
@@ -32,6 +32,7 @@ struct St_JudeApp: App {
                     }
                     .navigationBarTitleDisplayMode(.inline)
             }
+            .navigationViewStyle(.stack)
         }
     }
 }
