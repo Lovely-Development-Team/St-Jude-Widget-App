ed2b159ba005700eb73068b1bdb35efda7d0132c
Ben Cardy <ben.cardy@ocado.com>
2022-08-29 21:43:09 +0100

    Move main fundraising card to its own view

--- Shared/Views/CampaignList.swift
+++ Shared/Views/CampaignList.swift
@@ -45,30 +45,6 @@ struct CampaignList: View {
     @State private var compactListMode: Bool = false
     @State private var selectedCampaignId: UUID? = nil
     
-    @ViewBuilder
-    func mainProgressBar(value: Float, color: Color) -> some View {
-        ProgressBar(value: .constant(value), fillColor: color)
-            .frame(height: 15)
-            .padding(.bottom, 2)
-    }
-    
-    @ViewBuilder
-    func mainAmountRaised(_ value: Text) -> some View {
-        value
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            .minimumScaleFactor(0.7)
-            .lineLimit(1)
-            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-    }
-    
-    @ViewBuilder
-    func mainPercentageReached(_ value: Text) -> some View {
-        value
-            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-            .opacity(0.8)
-    }
-    
     func compareNames(c1: Campaign, c2: Campaign) -> Bool {
         if c1.name.lowercased() == c2.name.lowercased() {
             return c1.id.uuidString < c2.id.uuidString
@@ -118,39 +94,7 @@ struct CampaignList: View {
         
         ScrollView {
             VStack(spacing: 0) {
-                VStack(spacing: 0) {
-                    Text(fundraisingEvent?.name ?? "Relay FM for St. Jude 2022")
-                        .multilineTextAlignment(.leading)
-                        .font(.headline)
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                        .padding(.bottom, 2)
-                    Text(fundraisingEvent?.cause.name ?? "St. Jude Children's Research Hospital")
-                        .font(.subheadline)
-                        .multilineTextAlignment(.leading)
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                        .opacity(0.8)
-                        .padding(.bottom, 20)
-                    if let fundraisingEvent = fundraisingEvent {
-                        if let percentageReached =  fundraisingEvent.percentageReached {
-                            mainProgressBar(value: Float(percentageReached), color: fundraisingEvent.colors.highlightColor)
-                        }
-                        mainAmountRaised(Text(fundraisingEvent.amountRaised.description(showFullCurrencySymbol: false)))
-                        if let percentageReachedDesc = fundraisingEvent.percentageReachedDescription {
-                            mainPercentageReached(Text("\(percentageReachedDesc) of \(fundraisingEvent.goal.description(showFullCurrencySymbol: false))"))
-                        }
-                    } else {
-                        mainProgressBar(value: 0, color: .accentColor)
-                        mainAmountRaised(Text("PLACEHOLDER"))
-                            .redacted(reason: .placeholder)
-                        mainPercentageReached(Text("PLACEHOLDER"))
-                            .redacted(reason: .placeholder)
-                    }
-                }
-                .foregroundColor(.white)
-                .padding()
-                .background(fundraisingEvent?.colors.backgroundColor ?? Color(red: 13 / 255, green: 39 / 255, blue: 83 / 255))
-                .cornerRadius(10)
-                .padding()
+                FundraiserCardView(fundraisingEvent: fundraisingEvent, showDisclosureIndicator: true)
                 
                 Link("Visit the event!", destination: URL(string: "https://stjude.org/relay")!)
                     .font(.headline)
--- Shared/Views/FundraiserCardView.swift
+++ Shared/Views/FundraiserCardView.swift
@@ -8,13 +8,75 @@
 import SwiftUI
 
 struct FundraiserCardView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    let fundraisingEvent: FundraisingEvent?
+    let showDisclosureIndicator: Bool
+    
+    @ViewBuilder
+    func mainProgressBar(value: Float, color: Color) -> some View {
+        ProgressBar(value: .constant(value), fillColor: color)
+            .frame(height: 15)
+            .padding(.bottom, 2)
     }
-}
-
-struct FundraiserCardView_Previews: PreviewProvider {
-    static var previews: some View {
-        FundraiserCardView()
+    
+    @ViewBuilder
+    func mainAmountRaised(_ value: Text) -> some View {
+        value
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .minimumScaleFactor(0.7)
+            .lineLimit(1)
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+    }
+    
+    @ViewBuilder
+    func mainPercentageReached(_ value: Text) -> some View {
+        value
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            .opacity(0.8)
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text(fundraisingEvent?.name ?? "Relay FM for St. Jude 2022")
+                    .multilineTextAlignment(.leading)
+                    .font(.headline)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 2)
+                
+                if showDisclosureIndicator {
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                        .opacity(0.8)
+                }
+            }
+            Text(fundraisingEvent?.cause.name ?? "St. Jude Children's Research Hospital")
+                .font(.subheadline)
+                .multilineTextAlignment(.leading)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .opacity(0.8)
+                .padding(.bottom, 20)
+            if let fundraisingEvent = fundraisingEvent {
+                if let percentageReached =  fundraisingEvent.percentageReached {
+                    mainProgressBar(value: Float(percentageReached), color: fundraisingEvent.colors.highlightColor)
+                }
+                mainAmountRaised(Text(fundraisingEvent.amountRaised.description(showFullCurrencySymbol: false)))
+                if let percentageReachedDesc = fundraisingEvent.percentageReachedDescription {
+                    mainPercentageReached(Text("\(percentageReachedDesc) of \(fundraisingEvent.goal.description(showFullCurrencySymbol: false))"))
+                }
+            } else {
+                mainProgressBar(value: 0, color: .accentColor)
+                mainAmountRaised(Text("PLACEHOLDER"))
+                    .redacted(reason: .placeholder)
+                mainPercentageReached(Text("PLACEHOLDER"))
+                    .redacted(reason: .placeholder)
+            }
+        }
+        .foregroundColor(.white)
+        .padding()
+        .background(fundraisingEvent?.colors.backgroundColor ?? Color(red: 13 / 255, green: 39 / 255, blue: 83 / 255))
+        .cornerRadius(10)
+        .padding()
     }
 }
