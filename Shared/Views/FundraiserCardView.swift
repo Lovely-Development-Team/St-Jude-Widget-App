4902eec9860e2913bd5ed726c0b568682d46a6f5
Ben Cardy <ben.cardy@ocado.com>
2022-08-29 21:40:35 +0100

    Always show Milestones/Reward counts

---
+++ Shared/Views/FundraiserCardView.swift
@@ -0,0 +1,20 @@
+//
+//  FundraiserCardView.swift
+//  St Jude
+//
+//  Created by Ben Cardy on 29/08/2022.
+//
+
+import SwiftUI
+
+struct FundraiserCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FundraiserCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FundraiserCardView()
+    }
+}
