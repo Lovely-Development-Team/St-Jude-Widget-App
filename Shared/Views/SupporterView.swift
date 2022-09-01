ff7e97dd1f600e3aa8a8a80112bd8c8bb2c2577d
Ben Cardy <ben.cardy@ocado.com>
2022-09-01 09:15:20 +0100

    UI changes for support view

--- Shared/Views/SupporterView.swift
+++ Shared/Views/SupporterView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 
-
 class FetchSupporters: ObservableObject {
     @Published var supporters = [String]()
     init() {
@@ -34,26 +33,50 @@ struct SupporterView: View {
     
     var body: some View {
         let supporters = fetch.supporters
-        if (supporters.count > 0) {
-            VStack {
-                Text("Supporters")
-                    .font(.title)
-                    .fontWeight(.bold)
-                Text("Our thanks to these awesome people for donating $10 or more to our fundraiser:")
-                    .padding(.top, 2)
-                    .padding(.bottom, 10)
-                    .multilineTextAlignment(.center)
+        VStack {
+            Text("Supporters")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top)
+            Text("Our thanks to these awesome people for donating **$10** or more to our fundraiser!")
+                .padding(.top, 2)
+                .padding(.bottom, 10)
+                .multilineTextAlignment(.center)
+            Link("tildy.dev/stjude", destination: URL(string: "https://tildy.dev/stjude")!)
+                .padding(.top, -5)
+                .padding(.bottom, 10)
+                .allowsTightening(true)
+                .minimumScaleFactor(0.7)
+                .font(.body)
+                .foregroundColor(.blue)
+                .buttonStyle(PlainButtonStyle())
+            if (supporters.count > 0) {
                 ScrollView {
-                    ForEach(supporters.indices) {
-                        Text(supporters[$0])
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .padding(2)
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 0) {
+                        ForEach(supporters.indices) {
+                            Text(supporters[$0])
+                                .padding(4)
+                        }
                     }
+                    Image("Team_Logo_F")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 200)
+                        .padding()
                 }
-                
+            } else {
+                ProgressView()
+                    .padding(.top, 40)
+                    .padding(.bottom, 10)
+                Text("Loading ...")
+                    .padding(.bottom, 40)
             }
-            .padding(10)
+            Spacer()
         }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding(10)
+        .background(Color.secondarySystemBackground)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
