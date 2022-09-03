95e6701806154d48755b05237c537a7586bbdc96
Ben Cardy <ben.cardy@ocado.com>
2022-09-03 08:57:42 +0100

    Viticci/MVO

--- Shared/Views/EasterEggs/EasterEggDirectory.swift
+++ Shared/Views/EasterEggs/EasterEggDirectory.swift
@@ -71,4 +71,32 @@ let easterEggDirectory: [UUID: CampaignViewEasterEgg] = [
     UUID(uuidString: "8A17EE82-B90A-4ABA-A22F-E8CC7E8CF410")!: xRayManAndMrYellow,
     // Main Campaign
     UUID(uuidString: "8F4E607C-A117-4C11-9172-23D19C1BE96C")!: xRayManAndMrYellow,
+    // Viticci
+    UUID(uuidString: "A4C28DED-BDFB-4CAA-8CA5-3D04447FF8CB")!: CampaignViewEasterEgg(
+        left: AnyView(
+            Image("weirdfish")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 60, height: 60)
+                .rotationEffect(.degrees(-30))
+                .offset(x: -40)
+                .offset(y: 5)
+                .tapToWobble(anchor: .center)
+        ),
+        right: nil
+    ),
+    // MVO
+    UUID(uuidString: "1392EA16-1BD0-4F87-BA40-02F2CB8C1019")!: CampaignViewEasterEgg(
+        left: AnyView(
+                Image("jonycube")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80, height: 80)
+                    .rotationEffect(.degrees(-20))
+                    .offset(x: -40)
+                    .offset(y: 5)
+                    .tapToWobble()
+        ),
+        right: nil
+    )
 ]
