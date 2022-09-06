5ddba0a8ff9fc2e28152234395bda0d2aab7c932
Ben Cardy <ben.cardy@ocado.com>
2022-09-06 09:41:46 +0100

    Formatting fixes, only show image if it exists to avoid flash of white when first tapped

--- Shared/Views/EasterEggs/AnimatedImage.swift
+++ Shared/Views/EasterEggs/AnimatedImage.swift
@@ -11,33 +11,40 @@ struct AnimatedImage: View {
     @State private var image: Image?
     let imageNames: [String]
     @State private var tapped: Bool = false
-
+    
+#if !os(macOS)
+    let bounceHaptics = UIImpactFeedbackGenerator(style: .light)
+#endif
+    
     var body: some View {
-      Group {
-          if tapped {
-              image?
-                  .resizable()
-                  .scaledToFit()
-          } else {
-              Image(self.imageNames.first!)
-                  .resizable()
-                  .scaledToFit()
-          }
-      }
-      .onTapGesture {
-          tapped.toggle()
-          self.animate()
-      }
+        Group {
+            if let image = image, tapped {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                Image(self.imageNames.first!)
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+        .onTapGesture {
+#if !os(macOS)
+            if !tapped {
+                bounceHaptics.impactOccurred()
+            }
+#endif
+            tapped.toggle()
+            self.animate()
+        }
     }
     
     private func animate() {
-      var imageIndex: Int = 0
+        var imageIndex: Int = 0
         var timerLoops: Int = 50
-       
-    
+        
         Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { timer in
             if imageIndex < self.imageNames.count {
-                print(timerLoops)
                 self.image = Image(self.imageNames[imageIndex])
                 imageIndex += 1
                 timerLoops -= 1
