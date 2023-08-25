d5ba3d7572063ef16f24262de13c860c7b04bdf1
Ben Cardy <ben.cardy@ocado.com>
2023-08-25 20:34:45 +0100

    Change direct donate link

--- Shared/Views/TeamEventCardView.swift
+++ Shared/Views/TeamEventCardView.swift
@@ -66,7 +66,7 @@ struct TeamEventCardView: View {
                             Label("Share Event Link", systemImage: "link")
                         }
                         Button(action: {
-                            showShareLinkSheet = ShareURL(url: URL(string: "https://tiltify.com/@relay-fm/relay-fm")!)
+                            showShareLinkSheet = ShareURL(url: URL(string: "https://donate.tiltify.com/@relay-fm/relay-fm")!)
                         }) {
                             Label("Share Direct Donation Link", systemImage: "dollarsign")
                         }
