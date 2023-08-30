d4b4b305d78a0789f6b044fd4a62c9c80b5927a1
Justin Hamilton <jwhamilton99@gmail.com>
2023-08-29 20:58:29 -0400

    added fade out at the top of the about view scroll view

--- Shared/Views/AboutView.swift
+++ Shared/Views/AboutView.swift
@@ -66,6 +66,7 @@ struct AboutView: View {
                 }
                 .padding(.horizontal)
                 .padding(.bottom)
+                .padding(.top, 10)
                 .background {
                     Color.black
                         .mask {
@@ -83,7 +84,18 @@ struct AboutView: View {
                         }
                 }
             }
-            .padding(.top, -180)
+            .padding(.top, -190)
+            .mask {
+                VStack(spacing: 0) {
+                    LinearGradient(stops: [
+                        .init(color: .clear, location: 0),
+                        .init(color: .black, location: 1)
+                    ], startPoint: .top, endPoint: .bottom)
+                    .frame(height: 10)
+                    Color.black
+                }
+                .padding(.top, -190)
+            }
         }
         .overlay(alignment: .topTrailing) {
             Button(action: {
