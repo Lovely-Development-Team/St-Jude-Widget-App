7b5f233983bb10b4fccdb52d9758cbdc48282689
Ben Cardy <ben.cardy@ocado.com>
2023-09-04 20:12:30 +0100

    Fix totals for graph, make total copyable

--- Shared/Views/StJudeTotals.swift
+++ Shared/Views/StJudeTotals.swift
@@ -16,9 +16,9 @@ struct FundraisingDataPoint: Identifiable {
 }
 
 let PREVIOUS_TOTALS_RAISED: [FundraisingDataPoint] = [
-    .init(year: "2018", goal: 20000, total: 64701),
-    .init(year: "2019", goal: 75000, total: 312000),
-    .init(year: "2020", goal: 315000, total: 456000),
+//    .init(year: "2018", goal: 20000, total: 64701),
+    .init(year: "2019", goal: 75000, total: 315939.56),
+    .init(year: "2020", goal: 315000, total: 484000),
     .init(year: "2021", goal: 333333.33, total: 701220.26),
     .init(year: "2022", goal: 494840.18, total: 706397.1),
 ]
@@ -52,7 +52,7 @@ struct StJudeTotals: View {
     
     var body: some View {
         Chart(totalRaised) { total in
-            BarMark(x: .value("Year", total.year), y: .value("USD", total.total), width: 25)
+            BarMark(x: .value("Year", total.year), y: .value("USD", total.total), width: .ratio(0.6))
                 .foregroundStyle(Color.accentColor.opacity(total.year == "2023" ? 1 : 0.5))
             RectangleMark(x: .value("Year", total.year), y: .value("USD", total.goal), height: 1)
                 .foregroundStyle(Color.brandPurple)
