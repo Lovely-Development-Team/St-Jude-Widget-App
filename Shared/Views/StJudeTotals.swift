4258aaa5388cf005b2c88e205d605002c5075fc1
Ben Cardy <ben.cardy@ocado.com>
2023-09-06 09:25:01 +0100

    Better graph transitions

--- Shared/Views/StJudeTotals.swift
+++ Shared/Views/StJudeTotals.swift
@@ -16,7 +16,6 @@ struct FundraisingDataPoint: Identifiable {
 }
 
 let PREVIOUS_TOTALS_RAISED: [FundraisingDataPoint] = [
-//    .init(year: "2018", goal: 20000, total: 64701),
     .init(year: "2019", goal: 75000, total: 315939.56),
     .init(year: "2020", goal: 315000, total: 484000),
     .init(year: "2021", goal: 333333.33, total: 701220.26),
@@ -34,7 +33,7 @@ struct StJudeTotals: View {
     var currentTotal: Double = 293000
     
     @State private var lineGraph: Bool = false
-        
+    
     var totalRaised: [FundraisingDataPoint] {
         PREVIOUS_TOTALS_RAISED + [
             .init(year: "2023", goal: 293000, total: currentTotal)
@@ -54,22 +53,25 @@ struct StJudeTotals: View {
     
     var body: some View {
         Chart(totalRaised) { total in
-            if lineGraph {
-                PointMark(x: .value("Year", total.year), y: .value("USD", total.goal))
-                    .foregroundStyle(Color.brandPurple)
-                    .opacity(0.75)
-                RectangleMark(x: .value("Year", total.year), y: .value("USD", total.goal), width: .ratio(0.2), height: 1)
-                    .foregroundStyle(Color.brandPurple)
-                    .opacity(0.75)
-                LineMark(x: .value("Year", total.year), y: .value("USD", total.total))
-                    .foregroundStyle(Color.accentColor.opacity(total.year == "2023" ? 1 : 0.5))
-                PointMark(x: .value("Year", total.year), y: .value("USD", total.total))
-            } else {
-                RectangleMark(x: .value("Year", total.year), y: .value("USD", total.goal), height: 1)
-                    .foregroundStyle(Color.brandPurple)
-                BarMark(x: .value("Year", total.year), y: .value("USD", total.total), width: .ratio(0.6))
-                    .foregroundStyle(Color.accentColor.opacity(total.year == "2023" ? 1 : 0.5))
-            }
+            
+            PointMark(x: .value("Year", total.year), y: .value("USD", total.goal))
+                .foregroundStyle(Color.brandPurple)
+                .opacity(lineGraph ? 0.75 : 0)
+            RectangleMark(x: .value("Year", total.year), y: .value("USD", total.goal), width: .ratio(lineGraph ? 0.2 : 0), height: lineGraph ? 1 : 0)
+                .foregroundStyle(Color.brandPurple)
+                .opacity(0.75)
+            LineMark(x: .value("Year", total.year), y: .value("USD", lineGraph ? total.total : 0))
+                .opacity(lineGraph ? 1 : 0)
+                .foregroundStyle(Color.accentColor.opacity(total.year == "2023" ? 1 : 0.5))
+            PointMark(x: .value("Year", total.year), y: .value("USD", lineGraph ? total.total : 0))
+                .opacity(lineGraph ? 1 : 0)
+            
+            RectangleMark(x: .value("Year", total.year), y: .value("USD", total.goal), width: .ratio(lineGraph ? 0 : 0.75), height: 1)
+                .foregroundStyle(Color.brandPurple)
+                .opacity(lineGraph ? 0 : 1)
+            BarMark(x: .value("Year", total.year), y: .value("USD", lineGraph ? 0 : total.total), width: .ratio(0.6))
+                .foregroundStyle(Color.accentColor.opacity(total.year == "2023" ? 1 : 0.5))
+            
         }
         .chartYAxis {
             AxisMarks(
