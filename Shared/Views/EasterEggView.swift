33030f0fbb72a85152c73a3922c53624f57e77d6
Justin Hamilton <jwhamilton99@gmail.com>
2024-08-09 23:33:18 -0400

    random campaign picker

--- Shared/Views/EasterEggView.swift
+++ Shared/Views/EasterEggView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct EasterEggView: View {
+    @Environment(\.colorScheme) var colorScheme
     @Environment(\.openURL) var openURL
     @Environment(\.presentationMode) private var presentationMode
     
@@ -39,6 +40,76 @@ struct EasterEggView: View {
         Text("L2CU (\"Lovely to See You\") says \"\(affirmationToShow)\"")
     }
     
+    @ViewBuilder var topView: some View {
+        VStack(spacing:0) {
+            GroupBox {
+                VStack {
+                    Text("Hi there!")
+                        .font(.largeTitle)
+                    HStack(spacing: 5) {
+                        Button(action: {
+                            withAnimation {
+                                #if !os(macOS)
+                                selectionHaptics.selectionChanged()
+                                #endif
+                                self.showFullL2CUName.toggle()
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                withAnimation {
+                                    self.showFullL2CUName = false
+                                }
+                            }
+                        }) {
+                            Text(showFullL2CUName ? "Lovely to See You" : "L2CU")
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        Text("says:")
+                    }
+                    .padding(.top, 1)
+                    .font(.headline)
+                    Text("“\(affirmationToShow)”")
+                        .font(.title3)
+                        .multilineTextAlignment(.center)
+                        .allowsTightening(true)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .lineLimit(2)
+                        .fixedSize(horizontal: false, vertical: true)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+            }
+            .groupBoxStyle(BlockGroupBoxStyle())
+            .padding()
+            Spacer()
+            Button(action: {
+                withAnimation {
+                    #if !os(macOS)
+                    bounceHaptics.impactOccurred()
+                    #endif
+                    self.animate.toggle()
+                    self.animationType = .default
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.animate.toggle()
+                }
+            }) {
+                Image("Team_Logo_F")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth:500, maxHeight: 200)
+                    .scaledToFit()
+                    .accessibility(hidden: true)
+                    .offset(x: 0, y: animate ? -5 : 0)
+                    .animation(animate ? .easeInOut(duration: 0.15).repeatForever(autoreverses: true) : animationType)
+            }
+            AdaptiveImage.groundRepeatable(colorScheme: self.colorScheme)
+                .tiledImageAtScale(axis: .horizontal)
+        }
+        .background(alignment: .bottom) {
+            AdaptiveImage.skyRepeatable(colorScheme: self.colorScheme)
+                .tiledImageAtScale(axis: .horizontal)
+        }
+    }
+    
     var body: some View {
         #if os(macOS)
         HStack {
@@ -55,109 +126,141 @@ struct EasterEggView: View {
                   .padding(.vertical, 10)
         }
         #endif
-        VStack {
-            Text("Hi there!")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Button(action: {
-                withAnimation {
-                    #if !os(macOS)
-                    bounceHaptics.impactOccurred()
-                    #endif
-                    self.animate.toggle()
-                    self.animationType = .default
+        VStack(spacing:0) {
+            self.topView
+            VStack {
+                GroupBox {
+                    VStack {
+                        Text("Love our apps?  Support our fundraiser!")
+                            .allowsTightening(true)
+                        //                        .padding(.top, 10)
+                        //                        .padding(.bottom, -20)
+                        Link(destination: URL(string: "https://tildy.dev/stjude")!, label: {
+                            Text("tildy.dev/stjude")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                        })
+                        //                        .padding(.top, -5)
+                        //                        .padding(.bottom, 10)
+//                            .foregroundColor(.blue)
+//                            .buttonStyle(PlainButtonStyle())
+                        .buttonStyle(BlockButtonStyle(tint: .accentColor))
+                    }
                 }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    self.animate.toggle()
-                }
-            }) {
-                Image("Team_Logo_F")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth:500, maxHeight: 400)
-                    .padding(.bottom, 20)
-                    .padding(.horizontal, 30)
-                    .scaledToFit()
-                    .accessibility(hidden: true)
-                    .offset(x: 0, y: animate ? -5 : 0)
-                    .animation(animate ? .easeInOut(duration: 0.15).repeatForever(autoreverses: true) : animationType)
-            }
-                .buttonStyle(PlainButtonStyle())
-            HStack(spacing: 5) {
+                .groupBoxStyle(BlockGroupBoxStyle())
                 Button(action: {
-                    withAnimation {
-                        #if !os(macOS)
-                        selectionHaptics.selectionChanged()
-                        #endif
-                        self.showFullL2CUName.toggle()
+                    showSupporterSheet = true
+                }, label: {
+                    Text("Supporters")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+//                        .padding(10)
+//                        .padding(.horizontal, 20)
+//                        .background(Color.accentColor)
+//                        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+//                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+//                        .padding(.bottom)
+                })
+                .buttonStyle(BlockButtonStyle(tint: .accentColor))
+                GroupBox {
+                    VStack {
+                        Text("L2CU drawing by rhl_. \nRelay for St. Jude crafted with care by The Lovely Developers. ")
+                            .font(.caption)
+                            .multilineTextAlignment(.center)
+                            .allowsTightening(true)
+                            .minimumScaleFactor(0.7)
+                            .foregroundColor(.secondary)
+                        Button(action: {
+                            openURL(URL(string: "https://tildy.dev")!)
+                        }, label: {
+                            Text("tildy.dev")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                        })
+                        //                    .padding(.top, 5.0)
+                        //                    .padding(.bottom, 10)
+                        //                    .font(.body)
+                        //                    .foregroundColor(.blue)
+                        //                    .buttonStyle(PlainButtonStyle())
+                        .buttonStyle(BlockButtonStyle(tint: .accentColor))
                     }
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                        withAnimation {
-                            self.showFullL2CUName = false
-                        }
-                    }
-                }) {
-                    Text(showFullL2CUName ? "Lovely to See You" : "L2CU")
                 }
-                .buttonStyle(PlainButtonStyle())
-                Text("says:")
+                .groupBoxStyle(BlockGroupBoxStyle())
+                .padding(.bottom)
             }
-            .font(.headline)
-            .padding(.bottom, 5.0)
-            Text("“\(affirmationToShow)”")
-                .font(.title)
-                .multilineTextAlignment(.center)
-                .allowsTightening(true)
-                .frame(maxWidth: .infinity, alignment: .center)
-            
-//            Text("Love our apps?  Support our fundraiser at \n")
-//                .allowsTightening(true)
-//                .padding(.top, 10)
-//                .padding(.bottom, -20)
-//            Link("tildy.dev/stjude", destination: URL(string: "https://tildy.dev/stjude")!)
-//                .padding(.top, -5)
-//                .padding(.bottom, 10)
-//                .allowsTightening(true)
-//                .minimumScaleFactor(0.7)
-//                .font(.body)
-//                .foregroundColor(.blue)
-//                .buttonStyle(PlainButtonStyle())
+            .padding()
+            .background {
+                GeometryReader { geometry in
+                    AdaptiveImage(colorScheme: self.colorScheme, light: .undergroundRepeatable, dark: .undergroundRepeatableNight)
+                        .tiledImageAtScale(scale: Double.spriteScale)
+                        .frame(height:geometry.size.height + 1000)
+                        .animation(.none, value: UUID())
+                }
+            }
+//            Text("Hi there!")
+//                .font(.largeTitle)
+//                .fontWeight(.bold)
 //            Button(action: {
-//                showSupporterSheet = true
-//            }, label: {
-//                Text("Supporters")
-//                    .font(.headline)
-//                    .foregroundColor(.white)
-//                    .padding(10)
-//                    .padding(.horizontal, 20)
-//                    .background(Color.accentColor)
-//                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-//                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-//                    .padding(.bottom)
-//            })
-            Spacer()
-            Section{
-                Text("L2CU drawing by rhl_. \nRelay for St. Jude crafted with care by The Lovely Developers. ")
-                    .font(.caption)
-                    .multilineTextAlignment(.center)
-                    .allowsTightening(true)
-                    .minimumScaleFactor(0.7)
-                    .foregroundColor(.secondary)
-                Button("tildy.dev") {
-                        openURL(URL(string: "https://tildy.dev")!)
-                }
-                .padding(.top, 5.0)
-                .padding(.bottom, 10)
-                .allowsTightening(true)
-                .minimumScaleFactor(0.7)
-                .font(.body)
-                .foregroundColor(.blue)
-                .buttonStyle(PlainButtonStyle())
-            }
-            .padding(5.0)
+//                withAnimation {
+//                    #if !os(macOS)
+//                    bounceHaptics.impactOccurred()
+//                    #endif
+//                    self.animate.toggle()
+//                    self.animationType = .default
+//                }
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                    self.animate.toggle()
+//                }
+//            }) {
+//                Image("Team_Logo_F")
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(maxWidth:500, maxHeight: 400)
+//                    .padding(.bottom, 20)
+//                    .padding(.horizontal, 30)
+//                    .scaledToFit()
+//                    .accessibility(hidden: true)
+//                    .offset(x: 0, y: animate ? -5 : 0)
+//                    .animation(animate ? .easeInOut(duration: 0.15).repeatForever(autoreverses: true) : animationType)
+//            }
+//                .buttonStyle(PlainButtonStyle())
+//            HStack(spacing: 5) {
+//                Button(action: {
+//                    withAnimation {
+//                        #if !os(macOS)
+//                        selectionHaptics.selectionChanged()
+//                        #endif
+//                        self.showFullL2CUName.toggle()
+//                    }
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//                        withAnimation {
+//                            self.showFullL2CUName = false
+//                        }
+//                    }
+//                }) {
+//                    Text(showFullL2CUName ? "Lovely to See You" : "L2CU")
+//                }
+//                .buttonStyle(PlainButtonStyle())
+//                Text("says:")
+//            }
+//            .font(.headline)
+//            .padding(.bottom, 5.0)
+//            Text("“\(affirmationToShow)”")
+//                .font(.title)
+//                .multilineTextAlignment(.center)
+//                .allowsTightening(true)
+//                .frame(maxWidth: .infinity, alignment: .center)
+//            }
+//            .padding(5.0)
         }
-        .padding(.top, 30)
-        .padding(10)
+        .background {
+            Color.skyBackground
+        }
+//        .padding(.top, 30)
+//        .padding(10)
         .accessibilityElement(children: .ignore)
         .accessibility(label: accessibilityLabel)
         .sheet(isPresented: $showSupporterSheet) {
