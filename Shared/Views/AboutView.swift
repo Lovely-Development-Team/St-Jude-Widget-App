144730c8a9f78b394b17f77150c15d347b37d920
Ben Cardy <ben.cardy@ocado.com>
2024-08-12 15:40:54 +0100

    Improved Supporters view

--- Shared/Views/AboutView.swift
+++ Shared/Views/AboutView.swift
@@ -15,101 +15,75 @@ struct AboutView: View {
     @State private var backgroundColor: Color = .black
     
     var body: some View {
-        ZStack {
-            ScrollView {
-                VStack(spacing:0) {
-                    VStack {
-                        Image(.bannerForeground)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding(.bottom)
-                        AdaptiveImage.groundRepeatable(colorScheme: self.colorScheme)
-                            .tiledImageAtScale(axis: .horizontal)
-                    }
-                    .background {
-                        ZStack(alignment: .bottom) {
-                            Color.skyBackground
-                            AdaptiveImage(colorScheme: self.colorScheme, light: .skyRepeatable, dark: .skyRepeatableNight)
-                                .tiledImageAtScale(scale: Double.spriteScale, axis: .horizontal)
-                                .animation(.none, value: UUID())
-                        }
-                    }
-                    VStack {
-                        GroupBox {
-                            VStack() {
-                                Text("About St. Jude")
-                                    .font(.title3)
-                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                                Text("The mission of St. Jude Children’s Research Hospital is to advance cures, and means of prevention, for paediatric catastrophic diseases through research and treatment. Consistent with the vision of our founder Danny Thomas, no child is denied treatment based on race, religion or a family’s ability to pay.")
-                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                                //                            .padding()
-                                //                        .background(Color(uiColor: .systemGroupedBackground))
-                                //                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                                Text("Every year throughout the month of September, Relay raises money for St. Jude to help continue its mission. Read more about the reason why, and this year's fundraiser, over at 512pixels.net.")
-                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                                    .padding(.vertical)
-                                Link(destination: URL(string: "https://512pixels.net/2024/08/relay-st-jude-2024/")!) {
-                                    Text("Read Stephen's post")
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-                                    //                                .padding(10)
-                                    //                                .padding(.horizontal, 20)
-                                    //                                .background(Color.accentColor)
-                                    //                                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                                    //                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-                                }
-                                .buttonStyle(BlockButtonStyle(tint: .accentColor))
-                                //                        Text("Our thanks go to everybody who donates to St. Jude via our fundraiser:")
-                                //                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                                //                        Button(action: {
-                                //                            showSupporterSheet = true
-                                //                        }) {
-                                //                            Text("Supporters")
-                                //                                .font(.headline)
-                                //                                .foregroundColor(.white)
-                                //                                .padding(10)
-                                //                                .padding(.horizontal, 20)
-                                //                                .background(Color.accentColor)
-                                //                                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                                //                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-                                //                        }
-                                //                        HStack {
-                                //                            Spacer()
-                                //                            Link("tildy.dev", destination: URL(string: "https://tildy.dev")!)
-                                //                            Spacer()
-                                //                        }
+        ScrollView {
+            VStack(spacing:0) {
+                AboutViewHeader()
+                VStack {
+                    GroupBox {
+                        VStack {
+                            Text("About St. Jude")
+                                .font(.title3)
+                                .fullWidth()
+                            Text("The mission of St. Jude Children’s Research Hospital is to advance cures, and means of prevention, for paediatric catastrophic diseases through research and treatment. Consistent with the vision of our founder Danny Thomas, no child is denied treatment based on race, religion or a family’s ability to pay.")
+                                .fullWidth()
+                                .padding(.top)
+                            Text("Every year throughout the month of September, Relay raises money for St. Jude to help continue its mission. Read more about the reason why, and this year's fundraiser, over at 512pixels.net.")
+                                .fullWidth()
+                                .padding(.top)
+                            Link(destination: URL(string: "https://512pixels.net/2024/08/relay-st-jude-2024/")!) {
+                                Text("Read Stephen's post")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .fullWidth(alignment: .center)
                             }
+                            .buttonStyle(BlockButtonStyle(tint: .accentColor))
+                            .padding(.top)
                         }
-                        .groupBoxStyle(BlockGroupBoxStyle())
-                        
-                        GroupBox {
-                            VStack {
-                                Text("About the app")
-                                    .font(.title3)
-                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                                Text("This app was developed by a group of friends from around the world, who came together thanks to Relay's membership program.")
-                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                                Link(destination: URL(string: "https://tildy.dev/")!, label: {
-                                    Text("tildy.dev")
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-                                })
-                                .buttonStyle(BlockButtonStyle(tint: .accentColor))
-                            }
-                        }
-                        .groupBoxStyle(BlockGroupBoxStyle())
                     }
-                    .padding()
-                    .padding(.bottom, 60)
-                    .background {
-                        GeometryReader { geometry in
-                            AdaptiveImage(colorScheme: self.colorScheme, light: .undergroundRepeatable, dark: .undergroundRepeatableNight)
-                                .tiledImageAtScale(scale: Double.spriteScale)
-                                .frame(height:geometry.size.height + 1000)
-                                .animation(.none, value: UUID())
+                    .groupBoxStyle(BlockGroupBoxStyle())
+                    
+                    GroupBox {
+                        VStack {
+                            Text("About the app")
+                                .font(.title3)
+                                .fullWidth()
+                            Text("This app was developed by a group of friends from around the world, who came together thanks to Relay's membership program.")
+                                .fullWidth()
+                                .padding(.top)
+                            Link(destination: URL(string: "https://tildy.dev/")!, label: {
+                                Text("tildy.dev")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .fullWidth(alignment: .center)
+                            })
+                            .buttonStyle(BlockButtonStyle(tint: .accentColor))
+                            .padding(.top)
+                            Text("Our thanks go to everybody who donates to St. Jude via our fundraiser.")
+                                .fullWidth()
+                                .padding(.top)
+                            Button(action: {
+                                showSupporterSheet = true
+                            }) {
+                                Text("Supporters")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .fullWidth(alignment: .center)
+                            }
+                            .buttonStyle(BlockButtonStyle(tint: .accentColor))
+                            .padding(.top)
                         }
+                    }
+                    .groupBoxStyle(BlockGroupBoxStyle())
+                    
+                }
+                .padding()
+                .padding(.bottom, 60)
+                .background {
+                    GeometryReader { geometry in
+                        AdaptiveImage(colorScheme: self.colorScheme, light: .undergroundRepeatable, dark: .undergroundRepeatableNight)
+                            .tiledImageAtScale(scale: Double.spriteScale)
+                            .frame(height:geometry.size.height + 1000)
+                            .animation(.none, value: UUID())
                     }
                 }
             }
@@ -123,12 +97,6 @@ struct AboutView: View {
                 presentationMode.wrappedValue.dismiss()
             }, label: {
                 Image(systemName: "xmark")
-//                    .imageScale(.large)
-//                    .foregroundStyle(Color.gray.opacity(0.75))
-//                    .padding([.all], 7)
-//                    .background(Color.gray.opacity(0.75))
-//                    .clipShape(Circle())
-//                    .shadow(radius: 5)
             })
             .buttonStyle(BlockButtonStyle())
             .padding()
@@ -140,10 +108,34 @@ struct AboutView: View {
     }
 }
 
-struct AboutView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            AboutView()
+struct AboutViewHeader: View {
+    @Environment(\.colorScheme) var colorScheme
+    @State var cloudMoved: Bool = false
+    var body: some View {
+        VStack {
+            Image(.bannerForeground)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.bottom)
+            AdaptiveImage.groundRepeatable(colorScheme: self.colorScheme)
+                .tiledImageAtScale(axis: .horizontal)
+        }
+        .background {
+            ZStack(alignment: .bottom) {
+                Color.skyBackground
+                AdaptiveImage(colorScheme: self.colorScheme, light: .skyRepeatable, dark: .skyRepeatableNight)
+                    .tiledImageAtScale(scale: Double.spriteScale, axis: .horizontal)
+                    .animation(.none, value: UUID())
+                AdaptiveImage.cloud(colorScheme: self.colorScheme)
+                    .imageAtScale(scale: Double.spriteScale)
+                    .offset(y: -(Double.spriteScale * 450))
+                    .offset(x: cloudMoved ? -1000 : 200)
+            }
+            .onAppear {
+                withAnimation(.linear(duration: 250)) {
+                    cloudMoved = true
+                }
+            }
         }
     }
 }
