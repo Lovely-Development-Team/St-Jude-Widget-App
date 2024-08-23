355c106d137d7474d4685c773ccd93fabf7de510
Ben Cardy <ben.cardy@ocado.com>
2024-08-23 19:05:18 +0100

    prep for 2024

--- Shared/Networking/ApiClient.swift
+++ Shared/Networking/ApiClient.swift
@@ -10,8 +10,8 @@ import Combine
 import WidgetKit
 
 let TEAM_EVENT_VANITY = "+relay-for-st-jude"
-//let TEAM_EVENT_SLUG = "relay-for-st-jude-2024"
-let TEAM_EVENT_SLUG = "relay-fm-for-st-jude-2023"
+let TEAM_EVENT_SLUG = "relay-for-st-jude-2024"
+//let TEAM_EVENT_SLUG = "relay-fm-for-st-jude-2023"
 
 struct TiltifyRequest: Codable {
     let operationName: String
