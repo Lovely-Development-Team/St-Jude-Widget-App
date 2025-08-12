e59bbe0e8f1bccac9d39fa1120a69995a07558b4
Pierre-Luc Robitaille <pierreluc2002@gmail.com>
2025-08-12 14:50:24 -0400

    Made the random player on main screen

--- Shared/Helpers/Players.swift
+++ Shared/Helpers/Players.swift
@@ -5,6 +5,21 @@
 //  Created by Pierre-Luc Robitaille on 2025-08-11.
 //
 
+struct PlayerImage{
+    var BaseImage: ImageResource
+    var LightImage: ImageResource
+    var ThrowImage: ImageResource?
+    var FightImage: ImageResource
+    var StreetImage: ImageResource
+    var ThrowScale: Double?
+    var BaseScale: Double
+    var FigthScale: Double
+    var BottomPadding: Double
+    var isPaddingMirrored: Bool = false
+    var HorizontalPadding: Double = 15.0
+    var FightImageMirrored = false
+}
+
 enum Players: CaseIterable, Identifiable{
     var id: Self { self }
     
@@ -22,21 +37,21 @@ enum Players: CaseIterable, Identifiable{
                                            ThrowImage: .stephenDodgeSuit,
                                            FightImage: .stephenFighting,
                                            StreetImage: .stephenStreet,
-                                           ThrowScale: 0.125,
+                                           ThrowScale: 0.13,
                                            BaseScale: 0.10,
                                            FigthScale: 0.25, // TODO: Adjust this
-                                           isPaddingMirrored: true,
-                                           Padding: 15.0)
+                                           BottomPadding: 22.5,
+                                           isPaddingMirrored: true)
             
         case .myke : return PlayerImage(BaseImage: .mykeSuit,
                                         LightImage: .mykeLights,
                                         ThrowImage: .mykeThrowSuit,
                                         FightImage: .mykeFighting,
                                         StreetImage: .mykeStreet,
-                                        ThrowScale: 0.25,
+                                        ThrowScale: 0.24,
                                         BaseScale: 0.10,
-                                        FigthScale: 0.26,
-                                        Padding: 15.0) // TODO: Adjust this
+                                        FigthScale: 0.26, // TODO: Adjust this
+                                        BottomPadding: 6.8)
             
         case .casey : return PlayerImage(BaseImage: .caseySuit,
                                          LightImage: .caseyLights,
@@ -44,7 +59,8 @@ enum Players: CaseIterable, Identifiable{
                                          StreetImage: .caseyStreet,
                                          BaseScale: 0.10,
                                          FigthScale: 0.25,
-                                         Padding: 40.0)
+                                         BottomPadding: -5.3,
+                                         HorizontalPadding: 40.0)
             
         case .kathy : return PlayerImage(BaseImage: .kathySuit,
                                          LightImage: .kathyLights,
@@ -52,6 +68,7 @@ enum Players: CaseIterable, Identifiable{
                                          StreetImage: .kathyStreet,
                                          BaseScale: 0.10,
                                          FigthScale: 0.20,
+                                         BottomPadding: 26.8,
                                          FightImageMirrored: true)
             
         case .jason : return PlayerImage(BaseImage: .jasonSuit,
@@ -60,15 +77,16 @@ enum Players: CaseIterable, Identifiable{
                                          StreetImage: .jasonStreet,
                                          BaseScale: 0.10,
                                          FigthScale: 0.20,
-                                         isPaddingMirrored: true,
-                                         Padding: 24.0)
+                                         BottomPadding: -0.75,
+                                         isPaddingMirrored: true)
             
         case .brad : return PlayerImage(BaseImage: .bradSuit,
                                         LightImage: .bradLights,
                                         FightImage: .bradFighting,
                                         StreetImage: .bradStreet,
                                         BaseScale: 0.10,
-                                        FigthScale: 0.25)
+                                        FigthScale: 0.25,
+                                        BottomPadding: 1.5)
         }
     }
 }
--- Shared/Views/CampaignList.swift
+++ Shared/Views/CampaignList.swift
@@ -562,7 +562,9 @@ struct CampaignList: View {
                         topView
                         
                         HStack{
-                            
+                            StandingToThrowingView(player: competitors.first!, isMirrored:true)
+                            Spacer()
+                            StandingToThrowingView(player: competitors.last!)
                         }
                         Spacer()
                             .padding(10)
