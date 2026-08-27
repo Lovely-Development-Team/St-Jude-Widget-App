56535fd1c111e790ad8970e1876d2ac4329c74ef
Justin Hamilton <jwhamilton99@gmail.com>
2026-08-27 19:57:28 -0400

    added themed campaign picker switching + start of 2026 random campaign picker

---
+++ Shared/Theming/ViewTheming.swift
@@ -0,0 +1,8 @@
+//
+//  ViewTheming.swift
+//  St Jude (iOS)
+//
+//  Created by Justin Hamilton on 8/27/26.
+//
+
+import Foundation
---
+++ Shared/Views/App/Themes/2026/RandomCampaignPickerView2026.swift
@@ -0,0 +1,18 @@
+//
+//  RandomCampaignPickerView2026.swift
+//  St Jude (iOS)
+//
+//  Created by Justin Hamilton on 8/27/26.
+//
+
+import SwiftUI
+
+struct RandomCampaignPickerView2026: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomCampaignPickerView2026()
+}
