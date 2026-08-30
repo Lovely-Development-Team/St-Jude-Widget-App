f1e2fe60b6ffaee5016acb17435420fd99f69a00
Pierre-Luc Robitaille <pierreluc2002@gmail.com>
2026-08-30 16:41:35 -0400

    Remove todos

--- Shared Widgets/WidgetEntryView.swift
+++ Shared Widgets/WidgetEntryView.swift
@@ -47,7 +47,7 @@ struct WidgetEntryView : View {
         if let campaign = entry.campaign {
             EntryView(campaign: .constant(campaign), showMilestones: shouldShowMilestones, preferFutureMilestones: preferFutureMilestones, showFullCurrencySymbol: entry.configuration.showFullCurrencySymbol?.boolValue ?? false, showGoalPercentage: shouldShowGoalPercentage, showMilestonePercentage: shouldShowMilestonePercentage, appearance: entry.configuration.appearance, disableCombos: shouldDisableCombos)
                 .widgetURL(URL(string: campaign.widgetURL)!)
-                .environment(\.font, Font.body/* TODO: (disablePixelFont: shouldDisablePixelTheme)*/)
+                .environment(\.font, Font.body)
         } else {
             if #available(iOS 17.0, *) {
                 placeholderView
