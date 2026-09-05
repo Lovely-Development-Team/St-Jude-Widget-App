36a7fa4d08d0a66826b10d3dfd348dd5e445923b
David Stephens <git@dwrs.org>
2026-09-05 22:51:52 +0100

    Switch to receiving Live Activity updates using broadcast channels

--- Shared/Networking/Model.swift
+++ Shared/Networking/Model.swift
@@ -460,6 +460,10 @@ struct UpdatePushTokenRequestBody: Encodable {
     let environment: String
 }
 
+struct LiveActivityChannelResponse: Decodable {
+    let channelId: String
+}
+
 
 struct TiltifyMultiSearchQueryCampaignResult: Codable {
     let id: UUID
