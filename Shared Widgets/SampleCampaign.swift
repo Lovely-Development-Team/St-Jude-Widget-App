01eeacb2f1f77185678fb6f60ef760766dd13dcb
Justin Hamilton <jwhamilton99@gmail.com>
2026-09-09 17:22:58 -0400

    h2h widgets minus lock screen + in-app navigation

--- Shared Widgets/SampleCampaign.swift
+++ Shared Widgets/SampleCampaign.swift
@@ -8,8 +8,11 @@
 import Foundation
 
 let sampleUser = TiltifyUser(username: "Relay", slug: "relay-fm", avatar: nil)
+let sampleUser2 = TiltifyUser(username: "TheLovelyDevelopers", slug: "tildy", avatar: nil)
 
 let sampleCampaign = TiltifyWidgetData(from: TiltifyCampaign(publicId: UUID(), avatar: nil, goal: TiltifyAmount(currency: "USD", value: "100"), milestones: [], slug: "relay-st-jude-21", status: "published", team: nil, user: sampleUser, description: ". . .", totalAmountRaised: TiltifyAmount(currency: "USD", value: "3000"), name: "Relay for St. Jude", originalGoal: TiltifyAmount(currency: "USD", value: "100"), rewards: []))
+
+let sampleCampaign2 = TiltifyWidgetData(from: TiltifyCampaign(publicId: UUID(), avatar: nil, goal: TiltifyAmount(currency: "USD", value: "100"), milestones: [], slug: "tildy-for-st-jude", status: "published", team: nil, user: sampleUser2, description: ". . .", totalAmountRaised: TiltifyAmount(currency: "USD", value: "3500"), name: "The Lovely Developers", originalGoal: TiltifyAmount(currency: "USD", value: "100"), rewards: []))
 
 let sampleCampaignSingleMilestone = TiltifyWidgetData(from: TiltifyCampaign(publicId: UUID(), avatar: nil,goal: TiltifyAmount(currency: "USD", value: "333333.33"), milestones: [TiltifyMilestone(amount: TiltifyAmount(currency: "USD", value: "10000"), name: "A milestone!", publicId: UUID())], slug: "relay-st-jude-21", status: "published", team: nil, user: sampleUser, description: ". . .", totalAmountRaised: TiltifyAmount(currency: "USD", value: "19065.66"), name: "Relay for St. Jude", originalGoal: TiltifyAmount(currency: "USD", value: "100"), rewards: []))
 
